4b2d92a08ffcfa5a807f764b4928b59dc2e8e357
Pipsson <juliusmallya8420@gmail.com>
2023-09-08 14:13:30 +0300

    finishing of navbar setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+const Navbar = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex flex-1 justify-between gap-16 max-lg:hidden items-center">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Nav from "./Nav";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Services from "./Services";
+export {
+    Hero,
+    CustomerReviews,
+    Subscribe,
+    Nav,
+    SpecialOffer,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    Services
+}
