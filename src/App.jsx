2a567fde94561c559df7697b8c091e14a5a1f552
Pipsson <juliusmallya8420@gmail.com>
2023-09-08 09:37:47 +0300

    initial setup of projects, Sections and other parts completed setup

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const App = () => (
+  <main>
+    Nav
+    <section
+      className="xl:padding-1 wide:padding-r
+     padding-b"
+    >
+      Hero
+    </section>
+    <section
+      className="
+     padding
+    "
+    >
+      PopularProducts
+    </section>
+    <section
+      className="
+     padding
+    "
+    >
+      SuperQuality
+    </section>
+    <section
+      className="
+     padding-x py-10
+    "
+    >
+      Services
+    </section>
+    <section
+      className="
+     padding
+    "
+    >
+      SpecialOffer
+    </section>
+    <section
+      className="
+    bg-pale-blue
+     padding
+    "
+    >
+      CustomerReviews
+    </section>
+    <section
+      className="
+     padding-x 
+     sm:py-32 py-16 w-full
+    "
+    >
+      Subscribe
+    </section>
+    <section
+      className="
+     padding-x
+     bg-black 
+     pb-8
+     pb-t
+    "
+    >
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
